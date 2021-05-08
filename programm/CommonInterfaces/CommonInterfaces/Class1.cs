231132cf0dd91a13b29b915721aa44c0ea6cbaca
Paul S <paulschult@yahoo.de>
2021-05-08 18:04:25 +0200

    CommonInterfaces erstellt

---
+++ programm/CommonInterfaces/CommonInterfaces/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonInterfaces
+{
+
+    public interface IDataManagerPatient
+    {
+
+    }
+
+    public interface IDataManagerUser
+    {
+
+    }
+
+    public interface IUserManagement
+    {
+        int ID { get; }
+
+        string Username { get; }
+        
+        bool LogIn(string username, string password);
+
+        bool LogOut();
+
+        bool GetLoginStatus();
+
+    }
+
+    public interface IPatient
+    {
+
+    }
+
+    public interface IOCRManager
+    {
+
+    }
+}
