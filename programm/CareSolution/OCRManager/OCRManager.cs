3b8e262b5a09d4cc72b4d28e1e05e99cb4d9c147
Ala Al-Khazzan <s0573997@htw-berlin.de>
2021-05-17 19:46:49 +0200

    Klasse in OCRManager erzeugt, die die Schnittstelle implementiert.

---
+++ programm/CareSolution/OCRManager/OCRManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonInterfaces;
+
+namespace OCRManager
+{
+    public class OCRManager : IOCRManager
+    {
+        public Dictionary<string, string> ExecuteOCR(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
