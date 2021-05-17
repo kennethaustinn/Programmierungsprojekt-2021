0094169a7f91fca92a8f558f94e8dd74aa6b430c
Kenneth Austin <s0574052@htw-berlin.de>
2021-05-17 19:46:32 +0200

    Klassen in Patient erzeugt, die die Schnitstellen implementieren

--- programm/CareSolution/Patient/Class1.cs
+++ programm/CareSolution/Patient/Class1.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommonInterfaces;
 
 namespace Patient
 {
-    public class Class1
+    public class Class1 : DataManager.Patient, IPatient
     {
+        public double CalculateBMI()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string CheckWarning()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
