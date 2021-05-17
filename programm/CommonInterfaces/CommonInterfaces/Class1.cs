4a19e48078c66581c39099a71b54714f8f96b4df
Paul S <paulschult@yahoo.de>
2021-05-17 19:27:28 +0200

    Interface Kommentare hinzugefügt Interface Fehler bereinigt

--- programm/CommonInterfaces/CommonInterfaces/Class1.cs
+++ programm/CommonInterfaces/CommonInterfaces/Class1.cs
@@ -6,60 +6,61 @@ using System.Threading.Tasks;
 
 namespace CommonInterfaces
 {
+    // Die Datenstruktur (Eigenschaften/Felder) der Klassen sind über das Datenbankmodel in Datamanger erzeugt worden.
+    // Komponenten, die diese Datenstruktur verwenden, müssen von den Klassen im Datamanger erben und
+    // ihre jeweilige Schnittstelle implementieren.
 
     public interface IDataManager<T> where T: IDataBaseConform
     {
-        bool Update(T careSolutionObject); // darf nur Objekte von Person entgegennehmen, updatet vorhandenen DBObjekt
+        // generische Schnittstelle der Komponente Datamanger
+        // Kann Objekte verarbeiten, welche die Schnittstelle IDataBaseConform implementieren
+
+        // speichert/überschreibt das übergebene Objekt in der Datenbank ab. true = erfolgreich
+        bool Update(T careSolutionObject);
+
+        // erstellt einen neuen Datenbankeintrag für das übergebene Objekt, true = erfolgreich
         bool CreateNew(T careSolutionObject);
-        List<T> SearchPatient(string userSearchText);   //Nimmt den Übergabeparameter, sucht in der Datenbank bei Mensch/Patient mit dem Text und gibt Ergebnis (Objekt der Klasse) zurück als Ergebnis
 
-        // Case: Mehrere Ergebnisse erhalten bei der Suche?
-        // seachbyID, searchbyXY Methoden eventuell noetig
+        // Sucht mit dem übergebenen String in der Datenbank. Gibt als Ergebnis Liste von IDataBaseConform'en Objekten zurück
+        List<T> SearchPatient(string userSearchText);
     }
 
-    public interface IDataBaseConform  // identifiziert Klassen/Objecte, die von der Databank abgebildet werden können
+    public interface IDataBaseConform  
     {
-
+        // identifiziert Klassen/Objecte, die von der Databank abgebildet werden können
     }
-
-    /*
-     *  Oder public interface IUpdatable
-     * {
-     * bool Update();
-     * } und dann Implementierung von Update in jeder Klasse
-     */
-    
 
     public interface IUserManagement
     {
-        int ID { get; }         // Database Identifikator
+        // Interface für das Nutzer und Rechtemangement 
 
-        string Username { get; }    // Benutzername
 
-        bool LoggedIn { get; }     // gibt true zurück, wenn der Nutzer eingelogged ist
-        
-        bool LogIn(string username, string password);  // Funktion zum einloggen, prüft das eingegebene Passwort in der Datenbank bzw. deren Hash Code in der Datenbank und gibt True zurück bei Übereinstimmung
+        // Funktion zum einloggen, prüft das eingegebene Passwort in der Datenbank
+        // bzw. deren Hash Code in der Datenbank und gibt True zurück bei Übereinstimmung
+        bool LogIn(string username, string password);  
 
         bool LogOut();             // Funktion zum ausloggen, bei Erfolg True als Rückgabewert
 
-        int GetUserRights();      // Gibt die Nutzerrechte als Int zurück. 1 für Doctor 2 für Pfleger 3 für Pflegefachkraft
-
-
-
-
+        // Gibt die Nutzerrechte als Int zurück. 1 für Doctor 2 für Pfleger 3 für Pflegefachkraft
+        int GetUserRights();      
     }
 
 
     public interface IPatient
     {
-        double CalculateBMI();
+        // Interface für die Patientendaten
+        double CalculateBMI();  // Berechnet den BMI des Patienten
+
+        // prüft alle Vitalparameter des Patienten und gibt Warnungen bei Überschreitung des Grenzwertes als string zurück
+        string CheckWarning();  
     }
 
     public interface IOCRManager
     {
-
-        Dictionary<string, string> ExecuteOCR(string path);  // Nimmt den Pfad der Bildatei oder PFD Datei entgegeben.
-                                                             // Erkennt automatisch, welches Formular eingelesen wird und befüllt entsprechend ein Dictionary
+        // Interface für die Kommunikation mit dem OCR Manager
+        Dictionary<string, string> ExecuteOCR(string path);  
+        // Nimmt den Pfad der Bild oder PDF-Datei entgegeben.
+        // Erkennt automatisch, welches Formular eingelesen wird und befüllt entsprechend ein Dictionary
     }
 
 
--- programm/DataManager/DataManager/Person.cs
+++ programm/DataManager/DataManager/Person.cs
@@ -14,7 +14,7 @@ namespace DataManager
     
     public partial class Person
     {
-        public int PersonID { get; set; }
+        public int PersonID { get; private set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Gender { get; set; }
