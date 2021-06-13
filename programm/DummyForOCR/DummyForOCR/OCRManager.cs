2e74ddd6d24a3dc9df75c3f727dbba1d92185a58
Ala Al-Khazzan <s0573997@htw-berlin.de>
2021-06-13 23:18:10 +0200

    Auswahlmenü ChooseFile zum auswählen einer Datei, während der Laufzeit, erstellt. Damit man nicht immer das Programm neustartet und dies hilfreich für das Auswertungsprgramm ist.

--- programm/DummyForOCR/DummyForOCR/OCRManager.cs
+++ programm/DummyForOCR/DummyForOCR/OCRManager.cs
@@ -8,17 +8,18 @@ namespace DummyForOCR
 {
     public class OCRManager : IOCRManager
     {
-        private const string FileName = @"C:\Users\ala19\source\repos\grp02\digitalisierung der Pflege\programm\DummyForOCR\DummyForOCR\Files\Pictures\Vorlage1.png";
+        private string _fileName;
+
 
         private readonly List<string> _bioItemKeyList = new List<string>()
         {
-            "Schule:", "Beruf:", "Universität:", "Partnerschaft:", "Kinder:", "Freizeit:", "Familienmitglieder:", "Sprachen:",
+            "Schule:","Ausbildung:", "Beruf:", "Universität:", "Partnerschaft:", "Kinder:", "Freizeit:", "Familienmitglieder:", "Sprachen:",
         };
         
 
         private readonly Dictionary<string, string> _bioDictionary = new Dictionary<string, string>();
 
-        private readonly string[] _bioItems = new string[8];
+        private readonly string[] _bioItems = new string[9];
 
         private string _allResult;
         private string _extract;
@@ -27,12 +28,14 @@ namespace DummyForOCR
         private List<string> _valueStrings;
         private List<string> _handSource;
 
+        private int _keyForCompareDictionary;
+
 
         private void ExecuteOcr()                                // Eingabeparamter noch auf Path ändern.
         {
             OpenFile();                                         //später path & filename als Übergabeparameter wie das Klassendiagramm einfügen.
             SearchForKeys();
-            AddToDictionary();
+            //AddToDictionary();                               // es kommt zu einer Fehlermeldung, wenn man während der Laufzeit eine andere Datei auswählt -> Hashcode 
         }
 
         private static IronTesseract ConfigurationOCR()
@@ -57,7 +60,7 @@ namespace DummyForOCR
         {
             var ocr = ConfigurationOCR();
 
-            var input = new OcrInput(FileName);
+            var input = new OcrInput(_fileName);
             var ocrResult = ocr.Read(input);
             _allResult = ocrResult.Text;
             var allResultLines = ocrResult.Lines;
@@ -128,8 +131,9 @@ namespace DummyForOCR
             _handSource = _valueStrings;
             for (int highLevel = 0; highLevel < _handSource.Count; highLevel++)
             {
-                if (_handSource[highLevel].Equals("") || _handSource[highLevel].Equals("Biografie")
+                if (_handSource[highLevel].Equals("") || _handSource[highLevel].Equals("Biografie") || _handSource[highLevel].Equals("Biografie Bit")
                                                       || _handSource[highLevel].Equals("Schule:")
+                                                      || _handSource[highLevel].Equals("Ausbildung:")
                                                       || _handSource[highLevel].Equals("Beruf:")
                                                       || _handSource[highLevel].Equals("Universität:")
                                                       || _handSource[highLevel].Equals("Partnerschaft:")
@@ -153,6 +157,7 @@ namespace DummyForOCR
                 switch (choice)
                 {
                     case 0:
+                        ChooseFile();
                         ExecuteOcr();
                         break;
                     case 1:
@@ -177,11 +182,14 @@ namespace DummyForOCR
                         break;
                     case 5:
                         RemoveWordForComparison();
-                        for (int i = 0; i < _handSource.Count; i++)
+                        for (int i = 0; i < _handSource.Count; i++)                                 // Bedingung muss immer an der CompareSource angepasst werden.
                         {
                             var analysis = new AnalysisProgram();
-                            var cost =analysis. CalculateDistance(_handSource[i], analysis.CompareList[i]);
-                            Console.WriteLine("{0} -> {1} = {2}", _handSource[i], analysis.CompareList[i], cost);
+                            analysis.AddCompareList();
+                            
+                            var compareSource = analysis.CompareDictionary[_keyForCompareDictionary]; // letztere element wieder nicht erreichbar
+                            var cost =analysis.CalculateDistance(_handSource[i], compareSource[i]); // i+1, da "Schule" mitgelesen wird.
+                            Console.WriteLine("{0} -> {1} = {2}", _handSource[i], compareSource[i], cost);  
                         }
                         break;
                     default:
@@ -193,6 +201,93 @@ namespace DummyForOCR
             }
             // ReSharper disable once FunctionNeverReturns
         }
+
+
+        private void PrintImageMenu()
+        {
+            Console.WriteLine("\n================================================================================");
+            string[] menuItems =
+            {
+                "\n(00) Erste funktionierende Vorlage\n", 
+                "(01)\t", "(02)\t", "(03)\t", "(04)\t", "(05)\n", "(06)\t", "(07)\t", "(08)\t", "(09)\t", "(10)\n", "(11)\t",
+                "(12)\t", "(13)\t", "(14)\t", "(15)\n", "(16)\t", "(17)\t", "(18)\t", "(19)\t", "(20)\n",
+            };
+
+            for (int i = 0; i < menuItems.Length; i++)
+            {
+                Console.WriteLine(menuItems[i]);
+            }
+            Console.WriteLine("\n================================================================================\n");
+        }
+
+        private void ChooseFile()                        
+        {
+            while (true)
+            {
+                PrintImageMenu();
+                Console.WriteLine("Bitte, geben Sie die Nummer der gewünschten Datei ein:");
+                int choice = ReadUserInputForInt();
+                switch (choice)
+                {
+                    case 0:
+                        _fileName = @"C:\Users\ala19\source\repos\grp02\digitalisierung der Pflege\programm\DummyForOCR\DummyForOCR\Files\Pictures\Vorlage1.png";
+                        _keyForCompareDictionary = 0;
+                        return;
+                    case 1:
+                        _fileName = @"C:\Users\ala19\Desktop\OCR\Auswertungsprogramm\Bio_Vorlage 1 P1.png";
+                        _keyForCompareDictionary = 1;
+                        return;
+                    case 2:
+                        _fileName = @"C:\Users\ala19\Desktop\OCR\Auswertungsprogramm\1.png";
+                        _keyForCompareDictionary = 2;
+                        return;
+                    case 3:
+                        _fileName = @"C:\Users\ala19\Desktop\OCR\Auswertungsprogramm\2.png";
+                        _keyForCompareDictionary = 3;
+                        return;
+                    case 4:
+                        return;
+                    case 5:
+                        break;
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+                    case 8:
+                        break;
+                    case 9:
+                        break;
+                    case 10:
+                        break;
+                    case 11:
+                        break;
+                    case 12:
+                        break;
+                    case 13:
+                        break;
+                    case 14:
+                        break;
+                    case 15:
+                        break;
+                    case 16:
+                        break;
+                    case 17:
+                        break;
+                    case 18:
+                        break;
+                    case 19:
+                        break;
+                    case 20:
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("Ungueltige Eingabe. Bitte ueberpruefen Sie Ihre Eingabe");
+                        }
+                        break;
+                }
+            }
+        }
+
 
         private void PrintMenue()
         {
