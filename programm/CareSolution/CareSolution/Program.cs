f9631c82e007de3c6cf551e49edf297a15de28be
Ala Al-Khazzan <s0573997@htw-berlin.de>
2021-07-12 22:48:40 +0200

    Unnötige Datein entfernt und Start Form auf Login gesetzt.

--- programm/CareSolution/CareSolution/Program.cs
+++ programm/CareSolution/CareSolution/Program.cs
@@ -16,7 +16,7 @@ namespace CareSolution
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new Login());
         }
     }
 }
